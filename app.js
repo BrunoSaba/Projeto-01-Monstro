3b74fca770dd2aa4313e342cf10862dc56aee63c
Bruno Silva Saba <brunosabah.silva@gmail.com>
2020-09-06 12:50:57 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 new Vue ({
     el: '#app',
     data: {
-
+        playerLife: 0,
+        monsterLife: 22,
     },
     computed: {
+        hasResult() {
+             return this.playerLife == 0 || this.monsterLife == 0;
+        }
 
     },
     methods: {
 
     },
     watch: {
-        
-    } 
 
+    } 
 })
